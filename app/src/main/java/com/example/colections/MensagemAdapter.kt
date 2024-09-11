c4438108b66e0006f84322b5075dd8d491e36098
leonardo nascimento <leonardo.rigonini@gmail.com>
2024-09-10 23:44:58 -0300

    notifydataset

--- app/src/main/java/com/example/colections/MensagemAdapter.kt
+++ app/src/main/java/com/example/colections/MensagemAdapter.kt
@@ -3,23 +3,46 @@ package com.example.colections
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 class MensagemAdapter(
-    private val lista: List<Mensagem>
+    private val clique: (String) -> Unit
 ) : Adapter<MensagemAdapter.MensagemViewHolder>() {
 
+    private var listaMensagens = mutableListOf<Mensagem>()
+
+    fun atualizarLista(lista: MutableList<Mensagem>){
+//        listaMensagens.addAll(lista)
+        listaMensagens = lista
+        notifyDataSetChanged()
+    }
     inner class MensagemViewHolder(
         val itemView:View
     ): ViewHolder(itemView){
 
 //        val text_nome: TextView = itemView.findViewById(R.id.text_nome)
 //        val text_ultima: TextView = itemView.findViewById(R.id.text_ultima)
+
          val text_nome: TextView = itemView.findViewById(R.id.text_card_view)
          val text_ultima: TextView = itemView.findViewById(R.id.text_card_ultima)
+         val imagePerfil: ImageView = itemView.findViewById(R.id.image_card_perfil)
+         val cardView: CardView = itemView.findViewById(R.id.card_view_layout)
+
 //         val text_horario: TextView = itemView.findViewById(R.id.text_horario)
+
+        fun bind(mensagem: Mensagem){//Conectar com a interface
+            text_nome.text = mensagem.nome
+            text_ultima.text = mensagem.ultima
+
+            cardView.setOnClickListener{
+                clique(mensagem.nome)
+            }
+
+        }
 
     }
     // onCreateViewHolder -> Ao criar o ViewHolder -> Criar a Visualização
@@ -42,16 +65,23 @@ class MensagemAdapter(
     //Ao vincular os dados do ViewHolder
     override fun onBindViewHolder(holder: MensagemViewHolder, position: Int) {
 
-        val mensagem = lista[position]
-        holder.text_nome.text = mensagem.nome
-        holder.text_ultima.text = mensagem.ultima
+        val mensagem = listaMensagens[position]
+        holder.bind(mensagem)
+
+//        holder.text_nome.text = mensagem.nome
+//        holder.text_ultima.text = mensagem.ultima
 //        holder.text_horario.text = mensagem.horario
 
+        //Aplicar eventos de clique
+//        val context = holder.imagePerfil.context
+//        holder.imagePerfil.setOnClickListener{
+//            Toast.makeText(context,"Olá Mundo ${mensagem.nome}", Toast.LENGTH_SHORT).show()
+//        }
 
     }
     // getItemCount -> Recuperar o tamanho da lista
     override fun getItemCount(): Int {
-        return lista.size
+        return listaMensagens.size
     }
 
 
--- app/src/main/java/com/example/colections/RecyclerviewActivity.kt
+++ app/src/main/java/com/example/colections/RecyclerviewActivity.kt
@@ -1,8 +1,12 @@
 package com.example.colections
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -10,25 +14,54 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 class RecyclerviewActivity : AppCompatActivity() {
 
     private lateinit var RecyclerviewActivity: RecyclerView
+    private lateinit var btnAtualizar: Button
+    private lateinit var mensagemAdapter: MensagemAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recyclerview)
 
-        val lista = listOf(
+        val lista = mutableListOf(
             Mensagem("Leonardo", "Olá Mundo", "9:50"),
             Mensagem("Leonard", "Olá dfggggggggggggggggggggggggggggggggggdfgdfffffffffffffffffffffffffffffffffffffffffffffffffffffff", "9:49"),
-            Mensagem("Leonar", "Olá Mun", "9:48"),
+            Mensagem("Leonar", "Olá Mun", "9:48")
         )
 
         RecyclerviewActivity = findViewById(R.id.rv_lista)
-        RecyclerviewActivity.adapter = MensagemAdapter(lista)
+        btnAtualizar = findViewById(R.id.btn_atualizar)
+
+        mensagemAdapter = MensagemAdapter{nome ->
+            Toast.makeText(this, "Olá $nome", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ListViewActivity::class.java)
+            intent.putExtra("nome", nome)
+
+            startActivity(
+                intent
+            )
+        }
+        mensagemAdapter.atualizarLista(
+            lista
+        )
+
+        RecyclerviewActivity.adapter = mensagemAdapter
         RecyclerviewActivity.layoutManager = StaggeredGridLayoutManager(
             2,
             RecyclerView.VERTICAL,
             
         )
+        RecyclerviewActivity.addItemDecoration(
+            DividerItemDecoration(this, RecyclerView.VERTICAL)
+        )
 
+        btnAtualizar.setOnClickListener{
+
+            lista.add(
+                Mensagem("Nova Leonar", "Olá Mun", "9:48")
+            )
+            mensagemAdapter.atualizarLista(lista)
+
+        }
         //LinearLayout(XML e Código)
 //        RecyclerviewActivity.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
 //        RecyclerviewActivity.layoutManager
